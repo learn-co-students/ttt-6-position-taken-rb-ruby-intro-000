6acf25c4b2fb9da2ec72c4cacb7a4f80a974ae8f
Michelle L <limichelle21@gmail.com>
2016-08-02 11:29:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+
+  return false if [" ", "", nil].include?(board[index])
+  return true if ["X", "O"].include?(board[index])
+
+end

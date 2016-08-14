448198226c66ea8747da645245b73b4e67647446
Mykola Bespaliuk <kolkabes@gmail.com>
2016-08-14 12:53:11 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+    if board.length <= index
+      return true
+    end
+
+    element = board[index]
+    case element
+    when "X", "O"
+      return true
+    end
+
+    return false
+end

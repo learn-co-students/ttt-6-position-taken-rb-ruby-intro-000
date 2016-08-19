d0bb19b4413a069752a85908e76ec6d8d5c4aeb6
Alejandro Coca <amcocarojas@gmail.com>
2016-08-19 19:39:25 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken? (board, index)
+
+  if index == " " || index == "" || index == nil
+    return false
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end

cecb99d068a3ed1ce4b610f22f2b01c628cde528
Anthony Zeitlin <zeitlin1@gmail.com>
2016-08-20 16:56:48 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken?(board,index)
+
+if board[index] == " " || board[index] == nil || board[index] ==""
+  return false
+elsif board[index] == "X"
+  return true
+elsif board[index] == "O"
+  return true
+else
+  return true
+end
+end

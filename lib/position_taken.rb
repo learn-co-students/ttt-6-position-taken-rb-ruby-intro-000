aa27464ac7d9e816e7f737d21f0dda60f9be36a6
kurt cruickshank <fect11@netzero.net>
2016-09-02 23:57:53 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,39 @@
-# code your #position_taken? method here!
+# def position_taken?(board, index)
+#
+#
+#
+#   #board = ["X","nil","O"," "," "," "," "," "," "]
+#   #
+#   board = []
+#   board[index] == "" || board[index] == " "
+#
+# end
+
+#  board[index]("" == " ") #|| board[index]("X" == "O")
+def position_taken?(board, index)
+
+!(board[index].nil? || board[index] == " "|| board[index] == "")
+end
+
+
+  # || board[index] == "O" || board[index] =="X"
+
+  # if position_taken?
+  #   yield index
+  # else
+  #   puts index
+  # end
+
+
+  #board[index] == "X" || board[index] =="X"
+  #board[0] == "O" || board[8] == "O"
+  #board[index] == " " || !(board[index] == " ")
+
+# end
+# # def position_taken?(board, index)
+#    board = []
+#   #board[index] == "" || board[index] == " "#|| !(board[index] == " ")
+#    board[index] == "" ||  !(board[index] == " ")
+#
+#
+# end

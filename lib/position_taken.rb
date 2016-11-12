6722bd4774233df6912659dcf2c77a0941332d8f
dollie phillips <dolpalfl@gmail.com>
2016-11-12 23:20:58 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,10 @@
 def position_taken?(board, index)
-  board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
-  index = 1
-  !(board[index].nil? || board[index] == " " || board[index] == "")
+  # board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+  # index = 1
+  # !(board[index].nil? || board[index] == " " || board[index] == "")
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
 end

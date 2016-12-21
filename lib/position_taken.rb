ebfe0052adaea050f6a43ad3af6c97a5d49d79c6
Caroline Hatwell <caroline.hatwell@gmail.com>
2016-12-21 17:18:36 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
+def position_taken?(board,index)
+  #array_index = index-1
+  case board[index]
+  when ""
+    return false
+  when " "
+    return false
+  when nil
+    return false
+
+  else
+    return true
+  #else
+    #return nil
+  end
+end
+
+
 # code your #position_taken? method here!

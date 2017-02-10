4a05c4da7feab753e3f76994c99bb5c0dd14a899
zouhair khallaf <zouhairkhallaf@gmail.com>
2017-02-10 00:07:53 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, i)
+  if(board[i] == " " || board[i] == "" || board[i] == nil)
+    false
+  elsif (board[i] == "X" || board[i] == "O")
+    true
+  end
+end

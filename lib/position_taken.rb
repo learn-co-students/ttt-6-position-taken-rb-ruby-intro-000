8ea635855dffcf3ff7aebb84f2f5bb487f47b4d4
Bruno Ricard <bricard8@gmail.com>
2017-03-09 16:15:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
-# code your #position_taken? method here!
+board = ["X", "", "O", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(board,user_input)
+
+  # array_target_index = (user_input.to_i) - 1
+
+  if board[user_input] == "X" || board[user_input] == "O"
+    puts "position_taken? = true"
+    return true
+  else
+    puts "position_taken? = false"
+    return false
+  end
+
+end
+
+# position_taken?(board, 5) #0 to 8

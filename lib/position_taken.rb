de603d758c6dcdfc995d1108a5adaf516d6926e2
Brigit Rossbach <brigitrossbach@gmail.com>
2017-05-01 02:51:50 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken? (array,index_number)
+  if array[index_number] == ""
+    return false
+  elsif array[index_number] == " "
+    return false
+  elsif array[index_number] == nil
+    return false
+  else
+    return true
+  end
+end

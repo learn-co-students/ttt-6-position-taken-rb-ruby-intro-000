dbdfc094a9479ffb15da500e44890a9c9407c6ac
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2017-05-04 23:17:39 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, position)
+    if board[position.to_i-1] != "X"
+      board[position.to_i-1] = "X"
+    else puts "This space taken."
+    end
+end

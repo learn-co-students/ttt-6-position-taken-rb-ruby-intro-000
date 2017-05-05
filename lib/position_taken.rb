a051074cda7dcc7beef69c3d7c8ca0a674e0d265
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2017-05-05 01:15:23 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,12 @@
 # code your #position_taken? method here!
 
+board = [" "," "," "," "," "," "," "," "," "]
+
+
 def position_taken?(board, position)
-    if board[position.to_i-1] != "X"
-      board[position.to_i-1] = "X"
-    else puts "This space taken."
-    end
+  if board[position] == " " || board[position] == "" || board[position] == nil
+    false
+  elsif board[position] == "X" || board[position] == "O"
+    true
+  end
 end

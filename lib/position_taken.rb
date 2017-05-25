6cff2cbafa278e09b0c5901bab18cd2305c6e81f
Omar Torres <01omartorres@gmail.com>
2017-05-25 23:24:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board.empty?
+    return false
+  else
+    if board[index] == nil
+      return false
+    else
+      in_len = board[index].strip.length
+      if in_len != 0
+        return true
+      else
+        return false
+      end
+    end
+  end
+end

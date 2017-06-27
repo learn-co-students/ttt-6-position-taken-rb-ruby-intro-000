44ee0253e87b5a17ae50002b6fbcf6e36eae83cc
Di Livius <liviu.sirghea@yahoo.com>
2017-06-27 08:28:30 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,5 @@
 # code your #position_taken? method here!
+def position_taken?(board, pos)
+  !(board[pos] == " " || board[pos] == "" || board[pos] == nil)
+  #!(board[pos].nil? || board[pos].empty?)
+end

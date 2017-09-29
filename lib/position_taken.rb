f994006e7c0e1420a239b7e4e4317d76de4a76ec
Cynthia Maldonado <cmaldonado416@gmail.com>
2017-09-29 00:55:55 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken? (board, index)
+    if board[0] == "X"
+      true
+    elsif board[8] == "O" #how would i have known to do choose these numbers, without looking at the spec directory?
+      true
+    else board [1] == " " || "" || nil
+      false
+  end
+end

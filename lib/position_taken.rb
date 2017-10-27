a55848650dbb95ac10b2a58aea16bb71ecafe434
GG Learn <ozolsdeko@gmail.com>
2017-10-27 07:33:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  field = board[index]
+  if field == " " || field == "" || field == nil
+    false
+  else
+    true
+  end
+end

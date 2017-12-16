08d641747ec0d963dfb027e875341f7115ea4e85
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-16 02:27:30 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
+
 # code your #position_taken? method here!
+board = []
+
+input = gets.strip
+
+def input_to_index(index)
+  index.to_i-1
+end
+
+index =input_to_index(input)
+
+def position_taken?(board,index)
+  if board[index] == " "
+    return false
+  end
+end
+
+position_taken?(board,index)

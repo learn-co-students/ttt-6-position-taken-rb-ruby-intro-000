9b143e142877d31e4c7bbd51241df3b77e8fa762
David Z <davidzabran@gmail.com>
2018-01-11 23:00:07 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+
+# psition_taken method checks that a space is empty
+def position_taken?(board,index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X"
+    true
+  else board[index] == "O"
+    true
+  end
+end

55e21a518eb401a80319a7750802b7bfba04f812
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 02:46:09 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " " || board[index].to_s.empty?
+    return false
+  elsif board[index].eql?("X") || board[index].eql?("O")
+    return true
+  end
+end

9764888c8f14c0a03e5eccb156d99a601782a31b
growhan@gmail.com Sinha <growhan@gmail.com>
2018-05-02 14:06:19 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(array, inp)
+  if( array[inp] == "X" || array[inp] == "O")
+    return true
+  elsif ( (array[inp] == " ") || (array[inp] == ""))
+    return false
+  else
+    return false
+  end
+end

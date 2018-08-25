7e737d73376c1800d902bd3132f95c640e1d46e9
Sky Resendez <sky.resendez@gmail.com>
2018-08-25 00:40:02 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,6 @@
-# code your #position_taken? method here!
+def position_taken?(board, pos)
+  if board[pos] == " " || board[pos] == "" || board[pos] == nil
+    return false
+  end
+  return true
+end

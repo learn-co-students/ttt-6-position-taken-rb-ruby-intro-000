17f4da4eb40dbae371bef7af5ea175229b11bf1f
Maura Slavin <mauramslavin@gmail.com>
2018-09-05 01:27:43 +0000

    Done.

--- spec/position_taken_spec.rb
+++ spec/position_taken_spec.rb
@@ -10,35 +10,35 @@ describe '#position_taken? in lib/position_taken.rb' do
 
   it 'returns false if the board has an empty string " " in the desired index' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 0
+    index = 1
 
     expect(position_taken?(board, index)).to be(false)
   end
 
    it 'returns false if the board has an empty string "" in the index' do
     board = ["", " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 0
+    index = 1
 
     expect(position_taken?(board, index)).to be(false)
   end
 
   it 'returns false if the board has nil in the index' do
     board = [nil, " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 0
+    index = 1
 
     expect(position_taken?(board, index)).to be(false)
 
-    index = 1
+    index = 2
     expect(position_taken?(board, index)).to be(false)
   end
 
   it 'returns true if the board has a value of "X" or "O" in the index' do
     board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
 
-    index = 0
+    index = 1
     expect(position_taken?(board, index)).to be(true)
 
-    index = 8
+    index = 9
     expect(position_taken?(board, index)).to be(true)
   end
 end

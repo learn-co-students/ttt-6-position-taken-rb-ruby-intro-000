2256f1e3e0a520556c23ee438d1dfdaf5370d790
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-05 20:59:39 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+
+def position_taken?(board, position)
+  if board[position] == " " || board[position] == "" || board[position] == nil
+    false
+  else
+    true
+  end
+end

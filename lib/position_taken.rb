75ef2c11ad80cfbf20fda448f9642ee956d3d50e
A Hashim <ahashim983@yahoo.com>
2019-03-09 10:14:16 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
+
+def convert_to_board_position(user_position)
+  index = user_position.to_i-1
+  return index
+end
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " "|""|nil
+    return false
+  elsif board[index] == "X"|"O"
+    return true
+  end
+end

bf74949edb22c1b023669af306aa00c22f74a671
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 08:47:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, position)
-  if (board[position] == " " || board[position] == "")
+  if (board[position] == " " || board[position] == "" || board[position] == nil)
     return false
   else
     return true

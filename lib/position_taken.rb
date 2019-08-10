428a21c4e3e887743c85f1154ea27d0c8bd326c9
Eric Taylor <eric@ericscotttaylor.com>
2019-08-10 00:29:09 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  check = board[index]
+  if check == " " || check == "" || check == nil
+    false
+  else
+    true
+  end
+end

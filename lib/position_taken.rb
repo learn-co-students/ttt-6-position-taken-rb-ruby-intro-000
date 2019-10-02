9cb97f75095437015b28474b111b913be755f943
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-02 13:25:41 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, move)
+  if board[move] == " " || board[move] == "" || board[move] == nil
+    return false
+  elsif board[move] == "X" || board[move] == "O"
+  return true
+  end
+end

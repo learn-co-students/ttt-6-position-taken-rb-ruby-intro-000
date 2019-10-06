ba043199f1feddee0f560e08912499f49876ee1f
John Fazekas <john.a.fazekas@gmail.com>
2019-10-06 00:47:13 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  value = board[index]
+
+  if(value == nil)
+    value = ""
+  end
+
+  return value.strip != ""
+end

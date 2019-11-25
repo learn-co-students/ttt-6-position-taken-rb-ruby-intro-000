4df9d67c05305a78a899dce1bea5e0e2ae329680
stellar-class-3593 <csamanta95@gmail.com>
2019-11-25 18:59:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+
+notempty = nil
+  if (board[index] ==  nil || board[index] == "" || board[index] == " ")
+    notempty = false
+  else
+    notempty = true
+  end
+end

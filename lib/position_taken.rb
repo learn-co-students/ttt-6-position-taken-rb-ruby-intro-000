e3b5276157b67e5a14f006d9598741a82fa0732d
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 01:33:22 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+spot = nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    spot = false
+  else
+    spot = true
+  end
+end

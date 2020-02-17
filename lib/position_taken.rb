9a0e182f89e55e0bcacbc95c5acd2a629945cae6
German Cruz <cruzgerman216@yahoo.com>
2020-02-17 21:35:18 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,22 @@
 # code your #position_taken? method here!
+
+def position_taken(arr, int)
+  if arr[0] == int
+    true
+  elsif arr[1] == int
+    true
+  elsif arr[2] == int
+    true
+  elsif arr[3] == int
+    true
+  elsif arr[4] == int
+    true
+  elsif arr[5] == int
+    true
+  elsif arr[6] == int
+    true
+  elsif arr[7] == int
+    true
+  elsif arr[8] == int
+    true
+end 

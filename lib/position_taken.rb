35838e25434e65a5ac9b304e4f49fb5a572c0723
rouan.thompson <rouan.thompson@gmail.com>
2020-02-21 04:59:15 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken?(array, index)
+  if array[index] == " "
+    false
+  elsif array[index] == ""
+    false
+  elsif array[index] == nil
+    false
+  else
+    true
+  end
+end

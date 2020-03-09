9d1520633d8993c70a47d411d92a56efcd25115a
Benjamin Ko <benjamintko@gmail.com>
2020-03-09 19:03:18 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,25 +1,33 @@
 # code your #position_taken? method here!
 
-# HOW COME THIS DOESN'T WORK?????
-#def position_taken?(board, index)
-#  if board[index] == " " || "" || nil
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+
+# This worked, but above is shorter and cleaner. 
+# def position_taken?(board, index)
+#  if board[index] == " "
+#   false
+#  elsif board[index] == ""
+#   false
+#  elsif board[index] == nil
+#   false
+#  else board[index] == "X" || "O"
+#   true
+#  end
+# end
+
+
+# This didn't work because for comparison seperated by || has to be defined again what it is comparing to.
+# def position_taken?(board, index)
+#  if board[index] == " " || == "" || == nil
 #    false
 #  else board[index] == "X" || "O"
 #    true
 #  end
-#end
-
-
-
- def position_taken?(board, index)
-  if board[index] == " "
-   false
-  elsif board[index] == ""
-   false
-  elsif board[index] == nil
-   false
-  else board[index] == "X" || "O"
-   true
-  end
- end
- 
+# end

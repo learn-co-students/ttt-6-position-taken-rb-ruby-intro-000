f9fd3ed9983fa80ec07d471e0139e43fec6845ba
Lucas Souza <souza.lucas01@gmail.com>
2020-03-11 02:42:22 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,3 @@
 # code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def position_taken?(board, index) !(board[index].nil? || board[index] == " " || board[index] == "") end

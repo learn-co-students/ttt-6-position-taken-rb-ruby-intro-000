635083a191299497f40b3fc5883b904901d5a413
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 04:36:11 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
 # code your #position_taken? method here!
+board = [""]
+index = 0
+
+def position_taken?(array, index)
+ array.each.with_index(1) do |value, idx|
+   if value == "" || value == " " || value == nil
+     puts "false"
+     return false
+   elsif value == "X" || value == "O"
+     puts "true"
+     return true
+   end
+  end
+ end
+
+
+position_taken?(board, index)

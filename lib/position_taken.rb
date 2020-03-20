87773e1db52bbe233ff41b2e7bdddde9cf5ba636
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-20 21:18:44 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,4 @@
-# code your #position_taken? method here!
+def position_taken?(board,index)
+  return false if(board[index] == "" || board[index] == " " || board[index] == nil)
+  return true if(board[index] == "X" || board[index] == "O")
+end

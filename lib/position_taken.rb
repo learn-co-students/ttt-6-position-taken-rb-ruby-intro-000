b6443cbda0a59a4416872f0d2df101642701f1b3
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-04 22:45:41 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,13 @@
 # code your #position_taken? method here!
-def position_taken?(index)
-  if index == ""
+def position_taken?(array,index)
+
+  if array[index] == ""
     false
-  elsif index == " "
-    false  
+  elsif array[index] == " "
+    false
+  elsif array[index] == nil
+    false
+  else
+    true
+  end
+end

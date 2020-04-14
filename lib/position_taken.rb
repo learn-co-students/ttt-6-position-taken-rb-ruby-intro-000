78701211b5f5b299c6c18661b56ec8a62dbe9424
William <william.l.munoz@gmail.com>
2020-04-14 00:28:06 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
+def position_taken?(board, index)
+
+if (board[index] == " " || board[index] == "" || board[index] == nil)
+  return false
+else
+  return true
+  end
+end
 # code your #position_taken? method here!

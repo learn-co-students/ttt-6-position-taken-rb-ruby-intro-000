7d501aa4afeb4c8bfab4330aa5bac5ff3027c0ba
Marc C <m.ciborowski@hotmail.com>
2020-05-21 20:31:50 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+def position_taken?(board = '" ", " ", " ", " ", " ", " ", " ", " ", " "', index = [0])
+  if board[0] == " "
+  false
+elsif board[0] == ""
+  false
+elsif board[0] == nil
+  false
+else board[0] == "X" || "O"
+  true
+  end
+end

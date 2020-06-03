26f01205ea5e6d282e2900af1bc411b1276106f7
Pete Ngai <sendccme55@gmail.com>
2020-06-03 19:48:34 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+def position_taken? (board, index)
+
+  current_board_player = board[index]
+
+  if (current_board_player == " ") || (current_board_player == "")
+    false
+  elsif current_board_player == "X"
+    true
+  elsif current_board_player == "O"
+    true
+  else false
+  end
+
+end

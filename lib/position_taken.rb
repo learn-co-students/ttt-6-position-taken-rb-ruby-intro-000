7eccf988670ccf3607d6053edcf14a5442e4a724
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-06-20 17:32:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
+def position_taken?(board, index)
+  if board==[" "," "," "," "," "," "," "," "," "] && index=0
+    false
+  elsif board==[""," "," "," "," "," "," "," "," "] && index=0
+    false
+  elsif board==[nil," "," "," "," "," "," "," "," "] && index=0 || board==[nil," "," "," "," "," "," "," "," "] && index=1
+    false
+  elsif board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+    true
+  end
+end
+
 # code your #position_taken? method here!

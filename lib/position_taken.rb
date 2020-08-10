9ca662275271b25f8903ccff7f32d48a3aa03c71
munificent-forum-1695 <bunchshawn@gmail.com>
2020-08-10 17:21:22 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
+
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  else
+    true
+  end
+end
 # code your #position_taken? method here!

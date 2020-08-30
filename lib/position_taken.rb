703626ac24533a3da57b3b46ddb1d38d279831a8
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-30 21:37:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "X" or board[index] ==  "O"
+  true
+  elsif board[index] == " " or "" or nil
+    false
+  elsif board[index] != " " or ""
+    true
+
+
+end
+end

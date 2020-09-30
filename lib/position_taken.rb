1b28e8de265ec83412f5f02e8a5c6d50ac11261c
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-30 00:31:05 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+
+def position_taken?(board,index_num)
+
+
+
+if board[index_num] == " " || board[index_num] == "" || board[index_num] == nil
+  return false
+else
+  return true
+end
+
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]

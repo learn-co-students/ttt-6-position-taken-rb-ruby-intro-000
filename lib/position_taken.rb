837a46b077f886ea81d2a459ab47ca7cd0e02b31
Mona Javed Syed <monajaved93@gmail.com>
2020-10-16 04:39:20 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
+  if board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
     return false
-  else board[index] == "X" || board[index] == "O"
+  else board[index_number] == "X" || board[index_number] == "O"
     return true
   end
 end

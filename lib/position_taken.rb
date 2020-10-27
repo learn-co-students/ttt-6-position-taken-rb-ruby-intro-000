33309c303698412bf7d7b435a444bd4bb008d5f3
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-10-27 02:49:40 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,24 @@
 # code your #position_taken? method here!
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def position_taken?(board,index)
+  if board[index]== " " || board[index]== "" || board[index]==nil
+    return false
+  else
+    return true
+end
+end
+
+  
+
+

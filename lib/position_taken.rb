709c60810e826e45f72857edb3baaa45bf90c709
Diane Compaore <dianeyasminec60@gmail.com>
2021-02-20 22:52:44 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+def position_taken? (board=[],index=0)
+  if board[index]== " " || board[index]== "" || board[index]== nil
+    return false
+  else
+    if board[index]=="X" || board[index]=="O"
+      return true
+    end
+  end
+
+end
+board=[1,"",2,4]
+position_taken?
